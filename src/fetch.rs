2fa76fd5577c045cd00a5900323f7f0457636971
Rob Ede <robjtede@icloud.com>
2023-11-07 01:11:44 +0000

    fix: prevent exiting too early on EoF errors

--- src/fetch.rs
+++ src/fetch.rs
@@ -5,10 +5,12 @@ use std::{
 };
 
 use der::Decode;
+use error_reporter::Report;
+use eyre::WrapErr;
 use rustls::{client::ServerCertVerifier, OwnedTrustAnchor, RootCertStore, ServerName};
 use x509_cert::Certificate;
 
-pub(crate) fn cert_chain(host: &str) -> Vec<Certificate> {
+pub(crate) fn cert_chain(host: &str) -> eyre::Result<Vec<Certificate>> {
     let mut root_store = RootCertStore::empty();
 
     root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
@@ -22,8 +24,7 @@ pub(crate) fn cert_chain(host: &str) -> Vec<Certificate> {
     let mut config = rustls::ClientConfig::builder()
         .with_safe_default_cipher_suites()
         .with_safe_default_kx_groups()
-        .with_protocol_versions(&[&rustls::version::TLS12])
-        .unwrap()
+        .with_protocol_versions(&[&rustls::version::TLS12])?
         .with_root_certificates(root_store)
         .with_no_client_auth();
 
@@ -31,41 +32,49 @@ pub(crate) fn cert_chain(host: &str) -> Vec<Certificate> {
         .dangerous()
         .set_certificate_verifier(Arc::new(NoopServerCertVerifier));
 
-    let server_name = ServerName::try_from(host).unwrap();
+    let server_name = ServerName::try_from(host)?;
 
-    let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
-    let mut sock = TcpStream::connect(format!("{host}:443")).unwrap();
+    let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name)?;
+    let mut sock = TcpStream::connect(format!("{host}:443"))?;
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
 
-    tls.write_all(
-        format!(
-            r#"GET / HTTP/1.1
+    let req = format!(
+        r#"GET / HTTP/1.1
 Host: {host}
 Connection: close
+User-Agent: inspect-cert-chain/{}
 Accept-Encoding: identity
 
-"#
-        )
-        .replace('\n', "\r\n")
-        .as_bytes(),
+"#,
+        env!("CARGO_PKG_VERSION"),
     )
-    .unwrap();
+    .replace('\n', "\r\n");
 
-    tls.flush().unwrap();
+    tracing::debug!("writing to socket:\n{req}");
+
+    tls.write_all(req.as_bytes())
+        .wrap_err("failed to write to socket")?;
+    tls.flush().wrap_err("failed to flush socket")?;
 
     let mut plaintext = Vec::new();
-    tls.read_to_end(&mut plaintext).unwrap();
+    match tls.read_to_end(&mut plaintext) {
+        Ok(_) => {}
+        Err(err) => {
+            tracing::warn!("failed to read from {host}: {}", Report::new(err));
+        }
+    }
 
     // peer_certificates method will return certificates by now
     // because app data has already been written
-    tls.conn
+    Ok(tls
+        .conn
         .peer_certificates()
         .map(|c| {
             c.iter()
                 .filter_map(|c| Certificate::from_der(&c.0).ok())
                 .collect()
         })
-        .unwrap_or_default()
+        .unwrap_or_default())
 }
 
 struct NoopServerCertVerifier;
