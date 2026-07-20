228b07618fadf47478695e2a805d3f54b45e340e
Rob Ede <robjtede@icloud.com>
2026-07-20 10:15:36 +0100

    chore(deps): upgrade RustCrypto certificate stack

--- src/util.rs
+++ src/util.rs
@@ -1,40 +1,9 @@
-#![allow(unused)]
-
-use std::io::Read;
-
-use chrono::TimeZone as _;
 use const_oid::{
     ObjectIdentifier,
-    db::{DB, Database, rfc5280, rfc5912, rfc6962},
+    db::{DB, rfc5280, rfc5912, rfc6962},
 };
 use itertools::Itertools as _;
-use x509_cert::spki::{AlgorithmIdentifier, AlgorithmIdentifierOwned};
-
-/// Taken from <https://github.com/rustls/rustls/blob/a71223c4/rustls/src/x509.rs>.
-pub(crate) fn wrap_in_asn1_len(bytes: &mut Vec<u8>) {
-    let len = bytes.len();
-
-    if len <= 0x7f {
-        bytes.insert(0, len as u8);
-    } else {
-        bytes.insert(0, 0x80u8);
-        let mut left = len;
-        while left > 0 {
-            let byte = (left & 0xff) as u8;
-            bytes.insert(1, byte);
-            bytes[0] += 1;
-            left >>= 8;
-        }
-    }
-}
-
-/// Prepend stuff to `bytes` to put it in a DER SEQUENCE.
-///
-/// Taken from <https://github.com/rustls/rustls/blob/a71223c4/rustls/src/x509.rs>.
-pub(crate) fn wrap_in_sequence(bytes: &mut Vec<u8>) {
-    wrap_in_asn1_len(bytes);
-    bytes.insert(0, u8::from(der::Tag::Sequence));
-}
+use x509_cert::spki::AlgorithmIdentifierOwned;
 
 #[track_caller]
 pub(crate) fn assert_null_params(alg: &AlgorithmIdentifierOwned) {
